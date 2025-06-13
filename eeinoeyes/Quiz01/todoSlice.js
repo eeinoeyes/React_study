5810666f28238d51d12e4d1f0c21307a901cc910
eeinoeyes <loveourjune@gmail.com>
2025-06-13 17:08:51 +0900

    up

---
+++ eeinoeyes/Quiz01/todoSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from '@reduxjs/toolkit'
+let nextId = 1
+
+const todoSlice = createSlice({
+   name: 'todos',
+   initialState: [],
+   reducers: {
+      addTodo: (state, action) => {
+         state.push({ id: nextId++, text: action.payload })
+      },
+      removeTodo: (state, action) => {
+         return state.filter((todo) => todo.id !== action.payload)
+      },
+   },
+})
+
+export default todoSlice.reducer
+export const { addTodo, removeTodo } = todoSlice.actions
